d4fee230288fc4c124b6f0859f819075f74bfd29
dwivediom00 <dwivediom00.com>
2022-10-19 22:38:54 +0530

    search is added

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ const user= require('../backend/routes/api/user')
 const doctor = require('../backend/routes/api/doctor');
 const appointment = require('../backend/routes/api/appointment');
 const profile = require('../backend/routes/api/profile')
+const search = require('../backend/routes/api/search')
 mongoose.connect(db )
 .then( () => {console.log("database connection established")} )
 .catch( (err) => {console.log("database connection failed",err)} );
+const cors = require('cors')
+app.use(cors())
+
 
 //router initalisze middleware
 app.use(express.json({ extended: false}));
@@ -19,6 +23,7 @@ app.use(express.json({ extended: false}));
  app.use("/api/doctor", doctor)
  app.use("/api/appointment", appointment)
  app.use("/api/profile",profile)
+ app.use("/api",search)
 
 const port = process.env.PORT || 5000;
 app.listen(port,() => {console.log(`server listening on ${port}`)});
---
+++ routes/api/search.js
@@ -0,0 +1,66 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+
+const router = express.Router();
+
+
+
+const User = require('../../model/User')
+const Doctor= require("../../model/Doctor")
+
+const Profile = require('../../model/Profile');
+
+const cors = require('cors')
+app.use(cors())
+
+
+router.get("/search/:key",(req,res)=>{
+      const keys = req.params.key; 
+      const data = Doctor.aggregate([
+        { $lookup:
+            {
+              from: 'profiles',
+              localField: '_id',
+              foreignField: 'doctor',
+              as: 'searchdata'
+            } 
+          },
+          {$unwind:"$searchdata"},
+          {$project:{
+            id: 1,
+            name: 1,
+            phone: 1 , 
+            clinic:"$searchdata.clinic",
+           
+            location: "$searchdata.location",
+            timing: "$searchdata.timing",
+            specialists: '$searchdata.specialists',
+            fees: '$searchdata.fees',
+          
+    
+          }},
+    
+          { $match : {
+            "$or":[
+                {"specialists":{$regex:keys}},
+                {"name":{$regex:keys}},
+                {" clinic":{$regex:keys}},
+               
+              
+              
+            ]} }
+       
+    ],function (error, data) {
+         console.log(data)    
+          res.json(data) 
+         
+     return (data);
+    
+    });
+
+
+})
+
+
+module.exports= router
